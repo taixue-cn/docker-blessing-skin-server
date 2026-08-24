4c86942057e383561c6ee0fd0d1ccdb78f959c4d
Chuanwise <chuanwise@qq.com>
2026-08-25 04:05:14 +0800

    ci: wait for smoke test readiness

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 apk --no-cache add curl
-curl --silent --fail http://app:8080 | grep 'PHP 8.1'
+curl --silent --show-error --fail \
+  --retry 15 --retry-connrefused --retry-delay 1 \
+  http://app:8080 | grep 'PHP 8.1'
